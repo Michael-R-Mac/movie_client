56ca0a3a6206a8a7ecfaa69262cd4239ff461f47
Michael <89353459+Michael-R-Mac@users.noreply.github.com>
2023-04-02 22:00:37 +0200

    Style update
    -install bootstrap
    -style all buttons from index.scss
    -add form to login and sign up
    -add container and rows to main view
    -add card style to movie card
    -set width of image to 100% in movie view
    -add primary and body color

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,22 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.Image} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director}</Card.Text>
+        <Button
+          className="back-button"
+          onClick={() => onMovieClick(movie)}
+          variant="link"
+        >
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
@@ -18,7 +26,7 @@ MovieCard.propTypes = {
     Image: PropTypes.string.isRequired,
     Description: PropTypes.string,
     Genre: PropTypes.string,
-    Director: PropTypes.string
+    Director: PropTypes.string,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequired,
 };
